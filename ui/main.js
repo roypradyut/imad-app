b9758f0d20be188189c7a1adc60757e7a58d05cd
roypradyut <roy.pradyut@gmail.com>
2018-04-08 16:14:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerText="New Value";
 
-var img = document.getElementById('madi');
+var img = document.getElementById('img');
 img.onclick = function(){
     img.style.marginLeft='100px';
 };
